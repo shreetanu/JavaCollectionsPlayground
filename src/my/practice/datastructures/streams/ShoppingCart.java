77744070ff3e408085a1fa5f3d83a93f207c16e9
Tanushree Aggarwal <47485195+shreetanu@users.noreply.github.com>
2025-01-31 16:28:48 +0530

    removing unnecessary imports

--- src/my/practice/datastructures/streams/ShoppingCart.java
+++ src/my/practice/datastructures/streams/ShoppingCart.java
@@ -1,7 +1,6 @@
 package my.practice.datastructures.streams;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
